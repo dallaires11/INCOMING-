59c984cd9c00f9de9a27416207f5f63f86f7f660
Sam Dallaire <waterfalldallaire@gmail.com>
2017-02-06 10:25:00 -0500

    Principe projectile

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 /**
  * Created by Chroon on 2017-01-20.
  */
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -8,11 +11,14 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 
 public class Main extends Application{
-    int x=0;
+    int x=50;
+    int proj=0;
+    boolean running=false;
     ArrayList<Projectile> projectiles=new ArrayList<>();
 
     @Override
@@ -23,25 +29,43 @@ public class Main extends Application{
         catapulte.setTranslateY(450);
         root.getChildren().add(catapulte);
         Scene jeu=new Scene(root, 1200,900);
+        Timeline phystique=new Timeline(new KeyFrame(Duration.millis(15),event->{
+            if(!projectiles.isEmpty()){
+                for(int y=0;y<projectiles.size();y++) {
+                        projectiles.get(y).accelerer();
+                        projectiles.get(y).setTranslateX(projectiles.get(y).getX());
+                        projectiles.get(y).setTranslateY(projectiles.get(y).getY());
+                }
+            }
+            //System.out.println("T");
+        }));
+        phystique.setCycleCount(Animation.INDEFINITE);
+        phystique.play();
         jeu.setOnKeyPressed(event ->{
             if(event.getCode()== KeyCode.SPACE){
                 x++;
+                //System.out.println(x);
             }
         });
         jeu.setOnKeyReleased(event ->{
             if(event.getCode()==KeyCode.SPACE){
-                System.out.println(x);
+                //System.out.println(x);
+                /*if(projectiles.size()==1){
+                    projectiles.clear();
+                    root.getChildren().remove(1);
+                }*/
                 projectiles.add(new Projectile(x));
-                x=0;
-
+                root.getChildren().add(projectiles.get(proj));
+                //projectiles.get(proj).setTranslateX(40);
+                //projectiles.get(proj).setTranslateY(450);
+                proj++;
+                x=50;
             }
         });
         primaryStage.setTitle("INCOMING!!!");
         primaryStage.setScene(jeu);
         primaryStage.show();
     }
-
-
 
 
     public static void main(String[] args) {
--- src/Projectile.java
+++ src/Projectile.java
@@ -1,13 +1,69 @@
-public class Projectile{
-    int x;
-    int y;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-    Projectile(int x){
+public class Projectile extends Group{
+    int angle,masse;
+    float x,y,force,vX,vY;
+    Circle forme;
+    HBox ensemble;
+    VBox label;
+    Label posX,posY,grav;
 
+    public Projectile(int puissance){
+        x=45;
+        angle=45;
+        y=450;
+        masse=30;
+        force=puissance;
+        double radian=Math.toRadians(angle);
+        vX =(float) (force/5 * Math.cos(radian));
+        vY =(float) (force/5 * Math.sin(radian));
+        forme=new Circle(x,y,10, Color.AQUAMARINE);
+        setLabel();
+        ensemble=new HBox(forme,label);
+        this.getChildren().add(ensemble);
+
+    }
+
+    public void setLabel(){
+        posX=new Label ("X: "+x);
+        posY=new Label ("Y: "+y);
+        grav=new Label ("9,8 m/s");
+        label=new VBox();
+        label.getChildren().addAll(posX,posY,grav);
+    }
+
+    private void bouger() {
+        x += vX/2;
+        y -= vY/2;
+
+        if(y>=450)
+            y=450;
+        posX.setText("X: "+x);
+        posY.setText("Y: "+y);
+        //System.out.println(x+"holla"+y);
+
+    }
+
+    public void accelerer() {
+
+        vY -= 0.147/*masse*/;
+
+        bouger();
     }
 
 
 
 
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
 
 }
