b2b7a2f607e09c817ab8810bcef95ff4dd50f5f1
Sam Dallaire <waterfalldallaire@gmail.com>
2017-02-09 15:07:46 -0500

    server

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,13 +13,27 @@ public class Serveur {
     public static void main(String [] args) {
         ServerSocket serveur;
         Socket slave;
+        BufferedReader in;
+        PrintWriter out;
+        DataOutputStream out2;
 
 
         try {
-            serveur = new ServerSocket(80);
+            serveur = new ServerSocket(81);
             System.out.println(InetAddress.getLocalHost());
             slave = serveur.accept();
             System.out.println("Un zéro s'est connecté !");
+            //out = new PrintWriter(slave.getOutputStream());
+            out2=new DataOutputStream(slave.getOutputStream());
+            out2.writeInt(1000000000);
+            out2.flush();
+
+            int x;
+            float y;
+            String s="SAlut";
+            boolean g;
+            char t='a';
+
             serveur.close();
             slave.close();
 
