5cd6b75cb55faa151f73ad864c0dac8fe9874cdb
Sam Dallaire <waterfalldallaire@gmail.com>
2017-03-09 11:35:16 -0500

    How it should be

--- src/Protoype/Client/Client.java
+++ src/Protoype/Client/Client.java
@@ -1,4 +1,4 @@
-package Client;
+package Protoype.Client;
 
 import javafx.application.Application;
 import javafx.scene.Group;
--- src/Protoype/Serveur/Reception.java
+++ src/Protoype/Serveur/Reception.java
@@ -1,4 +1,4 @@
-package Serveur;
+package Protoype.Serveur;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
