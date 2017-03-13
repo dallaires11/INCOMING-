daa7f3920f9e88fbe2aa056d64ea99f13f693133
Vincent <vincent@boily.ca>
2017-03-13 12:29:22 -0400

    Ajout interface Client pour passer de la reception au thread JavaFX, finalisation de la reception.

---
+++ src/Final/Client/Controller/Passeur.java
@@ -0,0 +1,9 @@
+package Final.Client.Controller;
+
+/**
+ * Created by Vincent on 2017-03-13.
+ */
+public interface Passeur {
+
+    public void passe(int position, double x, double y);
+}
--- src/Final/Client/Controller/Reception.java
+++ src/Final/Client/Controller/Reception.java
@@ -1,12 +1,17 @@
 package Final.Client.Controller;
 
+import javafx.application.Platform;
+
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class Reception extends Thread{
+    Passeur passeur;
 
     String adresse;
     Socket socket;
@@ -26,7 +31,7 @@ public class Reception extends Thread{
             multicastSocket = new MulticastSocket();
             multicastSocket.joinGroup(InetAddress.getByName("224.0.6.0"));
 
-            bytes = new byte[64];
+            bytes = new byte[128];
             buffer = ByteBuffer.wrap(bytes);
 
             positionClient = buffer.getInt(socket.getInputStream().read());
@@ -35,6 +40,34 @@ public class Reception extends Thread{
             System.out.println(e);
         }
         return positionClient;
+    }
+
+    public void run(){
+        while (true) {
+            try {
+                multicastSocket.receive(new DatagramPacket(bytes, bytes.length));
+
+                int nombreDeProjectiles = buffer.getInt();
+
+                if (nombreDeProjectiles != 0) {
+
+                    for (int z = 0; z >= nombreDeProjectiles; z++) {
+                        final int position = z;
+                        final double x = buffer.getDouble();
+                        final double y = buffer.getDouble();
+
+                        Platform.runLater(() -> passeur.passe(position, x, y));
+                    }
+
+                    buffer.clear();
+                }
+
+                sleep(15);
+
+            } catch (IOException | InterruptedException e) {
+                System.out.println(e);
+            }
+        }
     }
 
     public Socket getSocket(){
