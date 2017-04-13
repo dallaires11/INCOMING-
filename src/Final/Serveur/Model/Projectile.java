32a4df6056ffbbf08a3942284a82db07235ee6ba
Sam Dallaire <waterfalldallaire@gmail.com>
2017-04-13 12:09:20 -0400

    Friction au sol

--- src/Final/Serveur/Model/Projectile.java
+++ src/Final/Serveur/Model/Projectile.java
@@ -1,14 +1,16 @@
 package Final.Serveur.Model;
 
 public class Projectile {
-    private int taille;
+    private int taille,resistanceMouvTerreDur;
     private float x,y,vitesseX,vitesseY;
     private double aeroX,aeroY,masse;
-    private boolean collision;
+    private boolean collision,sol;
 
     Projectile(double x, double y, int puissance,int taille, double angle){
+        resistanceMouvTerreDur=0;
         this.taille=taille;
         collision=false;
+        sol=false;
         setMasse(taille);
         setVitesseInitial(angle,puissance);
         setPositionInitial(x, y);
@@ -36,20 +38,29 @@ public class Projectile {
 
     private void bouger(){
         x += vitesseX/2;
-        y -= vitesseY/2;
+        if(!sol) {
+            y -= vitesseY / 2;
 
-        if(y>=1960)
-            y=1960;
+            if (y >= 1960) {
+                y = 1960;
+                sol = true;
+
+                resistanceMouvTerreDur=-8;
+                if (vitesseX<0)
+                    resistanceMouvTerreDur = resistanceMouvTerreDur*-1;
+            }
+        }
     }
 
     public void accelerer(){
         double vitXtemp = (double) vitesseX;
         double vitYtemp = (double) vitesseY;
+
         aeroX = 0.5*1.225*Math.pow(vitXtemp,2)*4*Math.PI*Math.pow(0.25,2)*0.47;
         aeroY = 0.5*1.225*Math.pow(vitYtemp,2)*4*Math.PI*Math.pow(0.25,2)*0.47;
         //Aero = 1/2*Masse volumique air 15 degre C*Vitesse^2*Aire objet (Demi-Sphere)*Coeffiscient surfacique
 
-        vitesseX -= (aeroX/masse)/60;
+        vitesseX -= ((aeroX/masse)+resistanceMouvTerreDur)/60;
         vitesseY -= (9.8+(aeroY/masse))/60;
 
         bouger();
